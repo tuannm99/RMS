9016401f4f6a321fc2161071814ee75c07c277d4
tuannm99 <gm.tuannm99@gmail.com>
2022-02-16 21:05:11 +0700

    add - constants and token schema

---
+++ api/src/core/db/schema/token.schema.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const { toJSON } = require('./plugins');
+const { tokenTypes } = require('../../../constants');
+
+const tokenSchema = mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: [
+        tokenTypes.REFRESH,
+        tokenTypes.RESET_PASSWORD,
+        tokenTypes.VERIFY_EMAIL,
+      ],
+      required: true,
+    },
+    expires: {
+      type: Date,
+      required: true,
+    },
+    //blacklisted: {
+    //type: Boolean,
+    //default: false,
+    //},
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+tokenSchema.plugin(toJSON);
+
+/**
+ * @typedef Token
+ */
+const Token = mongoose.model('Token', tokenSchema);
+
+module.exports = Token;
