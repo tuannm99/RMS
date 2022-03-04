c25cd53f742bacf07e9621e8ead0109d95702a46
Son <sonpthe130160@fpt.edu.vn>
2022-03-04 14:29:16 +0700

    add - docapi user

--- api/src/user/user.router.js
+++ api/src/user/user.router.js
@@ -109,3 +109,169 @@ module.exports = router;
  *       "stack": ".......",
  *     }
  */
+
+/**
+ * @api {put} /api/v1/users/:id 2. User detail
+ * @apiName User detail
+ * @apiGroup User
+ * @apiPermission admin, employee
+ *
+ * @apiHeader {String} Content-Type application/json
+ * @apiHeader {String} Authorization Bearer Token.....
+ *
+ * @apiQuery {String} [username] search name (match all not contain)
+ * @apiQuery {Number} [limit=10] limit the number of return data
+ * @apiQuery {Number} [page=1]   choosing page
+ * @apiQuery {String} [sortBy="createdAt:asc"] sort by any field of the return collection, Ex:<span> createdAt:asc|createdAt:desc </span>
+ *
+ * @apiParam (Param) {String} id
+ *
+ * @apisuccess {Array}    results
+ * @apiSuccess {String}   .username
+ * @apiSuccess {String}   .firstName
+ * @apiSuccess {String}   .lastName
+ * @apiSuccess {String}   .avatar
+ * @apiSuccess {String}   .email
+ * @apiSuccess {String}   .role
+ * @apisuccess {Date}     .createdAt
+ * @apiSuccess {Date}     .updatedAt
+ * @apiSuccess {String}   .id
+ * @apiSuccess {Object}   tokens
+ * @apisuccess {Number}   page
+ * @apiSuccess {Number}   limit
+ * @apiSuccess {Number}   totalPages
+ * @apiSuccess {Number}   totalResults
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "results": [
+ *             {
+ *                 "username": "vanngo",
+ *                 "email": "ngotrongvan1999@gmail.com",
+ *                 "firstName": "van",
+ *                 "lastName": "ngo",
+ *                 "avatar": "string",
+ *                 "role": "guest",
+ *                 "createdAt": "2022-03-02T02:06:53.274Z",
+ *                 "updatedAt": "2022-03-02T02:06:53.275Z",
+ *                 "id": "621ed13dfa3a15dc62ebce1c"
+ *             }
+ *         ],
+ *         "page": 1,
+ *         "limit": 10,
+ *         "totalPages": 1,
+ *         "totalResults": 3
+ *     }
+ *
+ * @apiError NotFound User not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "code": 400,
+ *       "message": "User not found",
+ *       "stack": ".......",
+ *     }
+ */
+
+/**
+ * @api {put} /api/v1/users/:id 3. Edit user by id
+ * @apiName edit user by id
+ * @apiGroup User
+ * @apiPermission admin, employee
+ *
+ * @apiHeader {String} Content-Type application/json
+ * @apiHeader {String} Authorization Bearer Token.....
+ *
+ * @apiParam (Param) {String} id
+ *
+ * @apiParam (Body) {String} username
+ * @apiParam (Body) {String} email
+ * @apiParam (Body) {String} firstName
+ * @apiParam (Body) {String} lastName
+ * @apiParam (Body) {String} avatar
+ * @apiParam (Body) {String} role
+ * @apiParamExample (Body) {json} Body-Example:
+ *    {
+ *        "username": "vanngo1",
+ *        "email": "vannthe1301642@fpt.edu.vn",
+ *        "firstName": "van",
+ *        "lastName": "ngo",
+ *        "avatar": "string",
+ *        "role": "admin"
+ *    }
+ *
+ * @apiSuccess {String}     username
+ * @apiSuccess {String}     email
+ * @apiSuccess {String}     firstName
+ * @apisuccess {String}     lastName
+ * @apiSuccess {String}     avatar
+ * @apiSuccess {String}     role
+ * @apiSuccess {Date}       createdAt
+ * @apiSuccess {Date}       updatedAt
+ * @apiSuccess {String}     id
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "username": "vanngo1",
+ *          "email": "vannthe1301642@fpt.edu.vn",
+ *          "firstName": "van",
+ *          "lastName": "ngo",
+ *          "avatar": "string",
+ *          "role": "admin",
+ *          "createdAt": "2022-03-02T07:44:24.761Z",
+ *          "updatedAt": "2022-03-02T07:44:24.761Z",
+ *          "id": "621f2058fa3a15dc62ebce4d"
+ *      }
+ **
+ * @apiError NotFound User not found
+ * @apiError NotFound User not found
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "code": 400,
+ *       "message": "User not found",
+ *       "stack": ".......",
+ *     }
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "code": 400,
+ *       "message": "User not found",
+ *       "stack": ".......",
+ *     }
+ */
+
+/**
+ * @api {delete} /api/v1/user/:id 3. Delete user by id
+ * @apiName delete user by id
+ * @apiGroup User
+ * @apiPermission admin, hiringManager
+ *
+ * @apiHeader {String} Content-Type application/json
+ * @apiHeader {String} Authorization Bearer Token.....
+ *
+ * @apiParam (Param) {String} id
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *
+ * @apiError NotFound User not found.
+ * @apiError NotFound User not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "code": 400,
+ *       "message": "User not found",
+ *       "stack": ".......",
+ *     }
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "code": 400,
+ *       "message": "User not found",
+ *       "stack": ".......",
+ *     }
+ */
