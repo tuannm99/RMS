24e308ef848e1e4b61b4912116ee5d006b20c4c0
tuannm99 <gm.tuannm99@gmail.com>
2022-03-07 22:18:12 +0700

    update - role needed for jobs and usser accounts

--- api/src/user/user.router.js
+++ api/src/user/user.router.js
@@ -1,34 +1,24 @@
 const express = require('express');
 
-// const { checkAuth } = require('../core/global.middleware');
-// const { ROLES } = require('../constants');
+const { checkAuth } = require('../core/global.middleware');
+const { ROLES } = require('../constants');
 const userController = require('./user.controller');
 
 // router
 const router = express.Router();
 
-router.get(
-  '/',
-  // checkAuth(ROLES.admin),
-  userController.getAllUsersHandler
-);
+router.get('/', checkAuth(ROLES.admin), userController.getAllUsersHandler);
 router.get(
   '/:id',
-  // checkAuth(ROLES.admin, ROLES.employee),
+  checkAuth(ROLES.admin, ROLES.employee, ROLES.hiringManager),
   userController.getUserHandler
 );
 router.put(
   '/:id',
-  // checkAuth(ROLES.admin, ROLES.employee),
+  checkAuth(ROLES.admin, ROLES.employee, ROLES.hiringManager),
   userController.updateUserHandler
 );
-router.delete(
-  '/:id',
-  // checkAuth(ROLES.admin, ROLES.employee),
-  userController.deleteUserHandler
-);
-
-// router.get('/users', checkAuth(ROLES.admin, ROLES.employee), userController.getUsersHandler);
+router.delete('/:id', checkAuth(ROLES.admin), userController.deleteUserHandler);
 
 module.exports = router;
 
