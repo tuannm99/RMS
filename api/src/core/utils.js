bc0abbc9a01c0e6ad0a580aa7cc8e6d1a21a9788
tuannm99 <gm.tuannm99@gmail.com>
2022-03-11 15:15:09 +0700

    remove - no need schema validation, mongoose do itself

--- api/src/core/utils.js
+++ api/src/core/utils.js
@@ -1,22 +1,3 @@
-const httpStatus = require('http-status');
-const Joi = require('joi');
-const ApiError = require('./apiError');
-
-exports.validate = (schema) => (req, res, next) => {
-  const validSchema = this.pick(schema, ['params', 'query', 'body']);
-  const object = this.pick(req, Object.keys(validSchema));
-  const { value, error } = Joi.compile(validSchema)
-    .prefs({ errors: { label: 'key' }, abortEarly: false })
-    .validate(object);
-
-  if (error) {
-    const errorMessage = error.details.map((details) => details.message).join(', ');
-    return next(new ApiError(httpStatus.UNPROCESSABLE_ENTITY, errorMessage));
-  }
-  Object.assign(req, value);
-  return next();
-};
-
 /**
  * Create an object composed of the picked object properties
  * @param {Object} object
