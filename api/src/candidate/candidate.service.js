0a90fd6fb53cea146fae493afa0e09fdd1ec328b
tuannm99 <gm.tuannm99@gmail.com>
2022-03-23 15:21:53 +0700

    update - delete candidate

--- api/src/candidate/candidate.service.js
+++ api/src/candidate/candidate.service.js
@@ -69,6 +69,15 @@ const deleteCandidateById = async (id) => {
   if (!candidate) {
     throw new ApiError(httpStatus.NOT_FOUND, 'No such candidate found');
   }
+  // delete candidate ref in Job
+  await Job.findByIdAndUpdate(
+    { _id: candidate.jobId },
+    {
+      $pull: {
+        candidateId: candidate._id,
+      },
+    }
+  );
 };
 
 module.exports = {
