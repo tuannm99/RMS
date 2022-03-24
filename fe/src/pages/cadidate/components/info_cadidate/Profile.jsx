0c6e1e834fcd672b73bc658e63c8d17690284320
DESKTOP-NB3KGN9\anhvu <ngotrongvan1999@gmail.com>
2022-03-24 13:24:04 +0700

    fix - profile cadidate

--- fe/src/pages/cadidate/components/info_cadidate/Profile.jsx
+++ fe/src/pages/cadidate/components/info_cadidate/Profile.jsx
@@ -4,38 +4,40 @@ import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { Row, Col, Button } from 'antd';
+import { Row, Col, Button, Upload } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 
 function Profile(props) {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   const [pdfFile, setPdfFile] = useState(null);
+  const [nameFile, setNameFile] = useState(null);
 
-  const [pdfError, setPdfError] = useState('');
   const allowedFiles = ['application/pdf'];
-  const handleFile = (e) => {
-    let selectedFile = e.target.files[0];
-      if (selectedFile && allowedFiles.includes(selectedFile.type)) {
-        let reader = new FileReader();
-        reader.readAsDataURL(selectedFile);
-        reader.onloadend = (e) => {
-          setPdfError('');
-          setPdfFile(e.target.result);
-        };
-      } else {
-        alert("Please choose file PDF!")
-      }
+
+  const getBase64 =(file, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(file);
+  }
+
+  const handleFile = (info) => {
+    console.log(info)
+    if(info && allowedFiles.includes(info.file.type)){
+        getBase64(info.file.originFileObj,fileUrl => setPdfFile([fileUrl] ))
+        setNameFile(info.file.originFileObj.name)
+    }else{
+        alert("Please choose PDF file!")
+    }
   };
+  console.log(pdfFile)
   return (
     <Row>
       <Col span={12}>
-        <form>
-          <input
-            type="file"
-            onChange={handleFile}
-          ></input>
-          {pdfError && <span className="text-danger">{pdfError}</span>}
-        </form>
+      <Upload onChange={handleFile} maxCount={1} fileList={pdfFile}>
+        <Button type='primary' icon={<UploadOutlined />}>Upload</Button>
+      </Upload>
+      {pdfFile && <>{nameFile}</>}
       </Col>
       <Col span={12}>
         <Button className='fr' type='primary'>UPDATE</Button>
@@ -44,13 +46,11 @@ function Profile(props) {
         {pdfFile && (
           <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.min.js">
             <Viewer
-              fileUrl={pdfFile}
+              fileUrl={pdfFile[0]}
               plugins={[defaultLayoutPluginInstance]}
             ></Viewer>
           </Worker>
         )}
-
-        {/* render this if we have pdfFile state null   */}
         {!pdfFile && <>No file is selected yet</>}
       </Col>
     </Row>
