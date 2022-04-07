c9c98bf25a68a251fb9823f5bef8a4ea8626683d
Viet <nguyenhaiviet199918@gmail.com>
2022-04-07 16:00:22 +0700

    update - dashboard

--- fe/src/services/dashboardServices.js
+++ fe/src/services/dashboardServices.js
@@ -2,10 +2,17 @@ import request from '../utils/request';
 
 const BASE_API = 'http://rms-fpt.ddns.net:5000/api/v1';
 
-//get jobs
+//get interview
 export function getAllInterview(params) {
   return request(`${BASE_API}/candidates/interview/all`, {
     method: 'GET',
     params: params,
+  });
+}
+
+//get data chart
+export function getDataChart() {
+  return request(`${BASE_API}/chart/101`, {
+    method: 'GET',
   });
 }
