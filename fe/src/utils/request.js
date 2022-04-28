4a4a620fb85b8f573b0aa07b9f8c41f129c59231
tuannm99 <gm.tuannm99@gmail.com>
2022-04-28 22:16:56 +0700

    update - axios request refresh token

--- fe/src/utils/request.js
+++ fe/src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import moment from 'moment';
 import { hasResponseError } from './utils';
+
 /**
  * set up axios configuation
  */
@@ -10,59 +12,66 @@ const request = axios.create({
   },
 });
 
-request.interceptors.request.use((config) => {
-  config.headers.Authorization = localStorage.getItem('token')
-    ? `Bearer ${localStorage.getItem('token')}`
-    : '';
-  return config;
+let refreshTokenRequest = null;
+let countDown = 0;
+request.interceptors.request.use(async (config) => {
+  let tokens = localStorage.getItem('token');
+  let expires = localStorage.getItem('expires');
+  let refreshToken = localStorage.getItem('refreshToken');
+
+  const now = new Date();
+  if (
+    // 20 second token expires
+    now.getTime() + 20000 > moment.utc(expires).toDate().getTime() &&
+    expires
+  ) {
+    refreshTokenRequest = refreshTokenRequest
+      ? refreshTokenRequest
+      : axios.post('http://rms-fpt.ddns.net:5000/api/v1/auth/refresh-token', {
+          refreshToken: refreshToken,
+        });
+
+    try {
+      const res = await refreshTokenRequest;
+      // reset token request for the next expiration
+      refreshTokenRequest = null;
+      if (res.status >= 200 && res.status < 300) {
+        localStorage.setItem('token', res.data.newToken.access.token);
+        localStorage.setItem('expires', res.data.newToken.access.expires);
+        localStorage.setItem('refreshToken', res.data.newToken.refresh.token);
+        config.headers.Authorization = `Bearer ${res.data.newToken.access.token}`;
+      }
+    } catch (e) {
+      // a dumb way to handle alert when multiple response error occur
+      if (countDown === 0) {
+        axios.post('http://rms-fpt.ddns.net:5000/api/v1/auth/logout', {
+          refreshToken: refreshToken,
+        });
+        alert('Account expires or error authentication, please login again!');
+        window.location.pathname = '/login';
+        localStorage.clear();
+        countDown = 2;
+      }
+      setInterval(() => {
+        if (countDown !== 0) {
+          countDown--;
+        }
+      }, 1000);
+    }
+    return config;
+  } else {
+    config.headers.Authorization = tokens ? `Bearer ${tokens}` : '';
+    return config;
+  }
 });
 
-request.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const { response, config } = error;
-    const { data, status, statusText } = response;
-    const refreshToken = localStorage.getItem('refreshToken');
-    if (response.status !== 401) {
-      return { data, status, statusText };
-    }
-    return axios
-      .post('http://rms-fpt.ddns.net:5000/api/v1/auth/refresh-token', {
-        refreshToken: refreshToken,
-      })
-      .then((res) => {
-        localStorage.setItem('token', res.data.newToken.access.token);
-        localStorage.setItem('refreshToken', res.data.newToken.refresh.token);
-      })
-      .then(() => {
-        config.headers.Authorization = localStorage.getItem('token')
-          ? `Bearer ${localStorage.getItem('token')}`
-          : '';
-        return request(config);
-      })
-      .catch((error) => {
-        if (error.response.status !== 429) {
-          axios
-            .post('http://rms-fpt.ddns.net:5000/api/v1/auth/logout', {
-              refreshToken: refreshToken,
-            })
-            .then(() => {
-              localStorage.clear();
-              window.location.pathname = '/login';
-              alert('Authentication, please login again!');
-            })
-            .catch((error) => {
-              if (error.response.status !== 429) {
-                localStorage.clear();
-                window.location.pathname = '/login';
-                alert('Authentication, please login again!');
-              }
-            });
-        }
-      });
-  }
-);
+const handleError = (error) => {
+  const { response = {} } = error;
+  const { data, status, statusText } = response;
+  return { data, status, statusText };
+};
+request.interceptors.response.use((response) => {
+  return response;
+}, handleError);
 
 export default request;
