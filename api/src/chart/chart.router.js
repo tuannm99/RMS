fdfdc4dd477f004af2bb16b0946a49a599baefcb
Son <sonpthe130160@fpt.edu.vn>
2022-05-02 10:30:14 +0700

    add - chart role

--- api/src/chart/chart.router.js
+++ api/src/chart/chart.router.js
@@ -4,6 +4,7 @@ const chartController = require('./chart.controller');
 
 router.get('/101', chartController.countJobByDepartment);
 router.get('/102', chartController.countSex);
+router.get('/103', chartController.countRole);
 router.get('/countCandidate', chartController.countCandidate);
 router.get('/countCandidateApproved', chartController.countCandidateApproved);
 router.get('/countCandidateRejected', chartController.countCandidateRejected);
